37efab74f650db29dbbdd86b2d327a5223b5311b
valery-nguyen <valery.nguyen@ucla.edu>
2019-05-28 19:24:07 -0400

    change github link url

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -10,7 +10,7 @@ class Footer extends React.Component {
           <p>2019 CoolBnb, a minimal clone of Airbnb.com </p>
             <div>
               <a href="https://www.linkedin.com/in/valeryn/" target="_blank">LinkedIn</a>|
-              <a href="https://github.com/valery-nguyen" target="_blank">GitHub</a>|
+              <a href="https://github.com/valery-nguyen/coolbnb" target="_blank">GitHub</a>|
               <a href="https://angel.co/valeryn" target="_blank">AngelList</a>
             </div>
         </div>
